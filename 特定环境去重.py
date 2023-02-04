847ff7568a955e9ae7fc4ee00f7d3fdafe3fd2f3
顶针 <75886707+dingzhen-vape@users.noreply.github.com>
2023-02-04 14:12:36 +0800

    Add files via upload

--- 特定环境去重.py
+++ 特定环境去重.py
@@ -2,9 +2,10 @@ import json
 import math
 import os
 import shutil
+import sys
 import time
 while True:
-    是否重命名 = input("Y：重命名，N：筛选,请输入你的指示：")
+    是否重命名 = input("Y：重命名，N：筛选,请输入你的指示(大写)：")
     # 要求路径
     路径 = input("输入路径:")
     # 计时
@@ -22,10 +23,6 @@ while True:
         距离的平方 = pow(差x, 2) + pow(差y, 2)
         距离 = math.sqrt(距离的平方)
         return 距离
-    # 重命名函数
-    def rename(path114514, oldname, newname):
-        shutil.copyfile(fr"{path114514}\{oldname}", fr"{path114514}\{newname}")
-        os.remove(fr"{path114514}\{oldname}")
     # 检查是否有为“筛选出来的文件”的文件夹
     if not os.path.exists(fr"{路径}\筛选出来的文件"):
         pass
@@ -34,17 +31,19 @@ while True:
             os.remove(fr"{路径}\筛选出来的文件")
         except:
             print("删除文件夹失败，请手动删除")
+            time.sleep(1)
+            sys.exit()
     # 算大致时间
     文件树 = os.listdir(路径)
     文件数量 = len(文件树)
     大致时间 = 0.0011064728141934 * 文件数量
     print(f"大致需要时间{大致时间}秒")
-    time.sleep(3)
+    time.sleep(1)
     # 筛选
-    if 是否重命名 == "N" or 'n':
+    if 是否重命名 == "N" or 是否重命名 == "n":
         筛选距离 = input("请输入筛选的距离(请不要输入其他字符）：")
         开始计时 = time.time()
-        for i in range(0, 文件数量):
+        for i in range(1, 文件数量):
             当前文件 = 文件树[i]
             if str(文件树[i]).count(".json") == 1:
                 try:
@@ -53,28 +52,31 @@ while True:
                         坐标1 = json内容["position"]
                         x1 = 坐标1[0]
                         y1 = 坐标1[2]
-                    for x in range(1, 文件数量):
+                    for x in range(2, 文件数量):
                         下一个文件 = 文件树[x]
-                        try:
-                            with open(fr"{路径}\{下一个文件}", encoding="UTF-8", mode="r") as f:
-                                json内容 = json.load(f)
-                                坐标2 = json内容["position"]
-                                x2 = 坐标2[0]
-                                y2 = 坐标2[2]
-                            if 计算距离(x1, x2, y1, y2) <= float(筛选距离):
-                                if not os.path.exists(fr"{路径}\筛选出来的文件"):
-                                    os.mkdir(fr"{路径}\筛选出来的文件")
-                                    shutil.copyfile(fr"{路径}\{下一个文件}", fr"{路径}\筛选出来的文件\{下一个文件}")
-                                    os.remove(fr"{路径}\{下一个文件}")
-                                    print(fr"筛选出{下一个文件}距离:[{int(计算距离(x1, x2, y1, y2))}M]")
-                                    筛选数 += 1
-                                else:
-                                    shutil.copyfile(fr"{路径}\{下一个文件}", fr"{路径}\筛选出来的文件\{下一个文件}")
-                                    os.remove(fr"{路径}\{下一个文件}")
-                                    print(fr"筛选出{下一个文件}距离:[{int(计算距离(x1, x2, y1, y2))}M]")
-                                    筛选数 += 1
-                        finally:
+                        if 当前文件 == 下一个文件:
                             pass
+                        else:
+                            try:
+                                with open(fr"{路径}\{下一个文件}", encoding="UTF-8", mode="r") as f:
+                                    json内容 = json.load(f)
+                                    坐标2 = json内容["position"]
+                                    x2 = 坐标2[0]
+                                    y2 = 坐标2[2]
+                                if 计算距离(x1, x2, y1, y2) <= float(筛选距离):
+                                    if not os.path.exists(fr"{路径}\筛选出来的文件"):
+                                        os.mkdir(fr"{路径}\筛选出来的文件")
+                                        shutil.copyfile(fr"{路径}\{下一个文件}", fr"{路径}\筛选出来的文件\{下一个文件}")
+                                        os.remove(fr"{路径}\{下一个文件}")
+                                        print(fr"筛选出{下一个文件}距离:[{int(计算距离(x1, x2, y1, y2))}M],源文件：{当前文件}")
+                                        筛选数 += 1
+                                    else:
+                                        shutil.copyfile(fr"{路径}\{下一个文件}", fr"{路径}\筛选出来的文件\{下一个文件}")
+                                        os.remove(fr"{路径}\{下一个文件}")
+                                        print(fr"筛选出{下一个文件}距离:[{int(计算距离(x1, x2, y1, y2))}M],源文件：{当前文件}")
+                                        筛选数 += 1
+                            except:
+                                pass
                 except:
                     pass
             else:
@@ -82,7 +84,7 @@ while True:
                 break
         print(f"筛选出{筛选数}个文件")
     # 重命名
-    if 是否重命名 == "Y" or 'y':
+    if 是否重命名 == "Y" or 是否重命名 == "y":
         文件名 = input("输入要替换的名称:")
         开始计时 = time.time()
         遍历文件 = os.walk(路径)
